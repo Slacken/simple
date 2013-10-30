e7934c68e111a8d1244fde6861392641c3ac2e72
Slacken <xinkiang@gmail.com>
2013-10-30 11:27:33 +0800

    实现语法分析器

--- parser.rb
+++ parser.rb
@@ -24,7 +24,6 @@ class LexicalAnalyzer < Struct.new(:string)
       while more_tokens?
         tokens.push(next_token)
       end
-      puts tokens.to_s
     end
   end
 
@@ -49,7 +48,8 @@ class LexicalAnalyzer < Struct.new(:string)
     /\A#{pattern}/.match(string)
   end
 
-  def rule_with_longest_match(rules_with_matches) # longest match considering variable name may contain keyword
+  # considering variable name may contain keyword
+  def rule_with_longest_match(rules_with_matches)
     rules_with_matches.max_by{|rule, match| match.to_s.length }
   end
 
@@ -58,5 +58,23 @@ class LexicalAnalyzer < Struct.new(:string)
   end
 end
 
-a = LexicalAnalyzer.new('if(a = 1)')
-a.analyze
+require './pda'
+start_rule = PDARule.new(1, nil, 2, '$', ['S','$'])
+# S<statement>, W<while>, A<assign>, E<expression>,L<less-than>, M<multiply>, T<term>
+symbol_rules = [
+  PDARule.new(2, nil, 2, 'S', ['W']), PDARule.new(2, nil, 2, 'S', ['A']), # S->W|A
+  PDARule.new(2, nil, 2, 'W',['w','(','E',')','{','S','}']), # W->w(E){S}
+  PDARule.new(2, nil, 2, 'A', ['v','=','E']), # A->v=E
+  PDARule.new(2, nil, 2, 'E', ['L']), # E->L
+  PDARule.new(2, nil, 2, 'L', ['M','<','L']), PDARule.new(2, nil, 2, 'L', ['M']), # L-> M<L | M
+  PDARule.new(2, nil, 2, 'M', ['T','*','M']),PDARule.new(2, nil, 2, 'M', ['T']), # M-> T*M | T
+  PDARule.new(2, nil, 2, 'T', ['n']),PDARule.new(2, nil, 2, 'T', ['v']) # T->n|v
+]
+token_rules = LexicalAnalyzer::GRAMMAR.map{|rule| PDARule.new(2, rule[:token], 2, rule[:token], [])}
+stop_rule = PDARule.new(2, nil, 3, '$', ['$'])
+
+rulebook = NPDARuleBook.new([start_rule, stop_rule] + symbol_rules + token_rules)
+npda_design = NPDADesign.new(1, '$', [3], rulebook)
+
+token_string = LexicalAnalyzer.new('while(x < 5){x = x * 3}').analyze.join
+puts npda_design.accepts? token_string
